13ec385ab32ca486e91bcb870093e540ccf6a38c
Josh-Sharples <sharplesjd15@gmail.com>
2024-02-19 18:12:49 +0000

    this commit adds the home page

---
+++ src/Screens/home-page.tsx
@@ -0,0 +1,30 @@
+import { Button, Colors, View, Text, Image } from "react-native-ui-lib"
+import { StyleSheet, ImageBackground } from "react-native"
+import background from "../Images/coffee-background.jpeg"
+
+export default function Homepage() {
+
+
+  return (
+    <View style={styles.root}>
+      <ImageBackground source={background} style={styles.container} imageStyle={styles.image}>
+      <Text text30BO color={'#bf6240'}>Java Rewards</Text>
+      <Image source={''}/>
+      <Button label={'Customer'} size={Button.sizes.large} backgroundColor={'#bf6240'} text60BO/>
+      <Button label={'Business'} size={Button.sizes.large} backgroundColor={'#bf6240'} text60BO/>
+      </ImageBackground>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: { flex: 1, width: '100%'},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  image: {
+    opacity: .2
+  }
+})
