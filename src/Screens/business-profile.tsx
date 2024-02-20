45e135483578a9f884c28de80d74dce213dca3cc
Simon Tam <simontam19@outlook.com>
2024-02-20 15:00:13 +0000

    Update business profile screen

--- src/Screens/business-profile.tsx
+++ src/Screens/business-profile.tsx
@@ -1,8 +1,9 @@
 
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
-import { Card } from '@rneui/themed';
+import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity} from 'react-native';
+import { Card, Button } from '@rneui/themed';
 import { LineChart } from 'react-native-chart-kit';
 import profile from '../Images/7LUyy3-LogoMakr (1).png';
+
 
 
 
@@ -13,57 +14,61 @@ export default function BusinessProfile() {
                 <View>
                 <Image style={styles.avatar} source={profile}/>
                 </View>
-                <Text>"hello"</Text>
+                <Text style={styles.businessName}>Business Name</Text>
             </View>
             <View style={styles.bio}>
-                    <Card>
-                        <Card.Title>"My Bio"</Card.Title>
-                        <Card.Divider/>
+                    <Card containerStyle={{borderRadius: 8}}>
+                        <Card.Title>Your bio</Card.Title>
                         <Text>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."</Text>
+                    <Button title="Update bio" containerStyle={styles.button} 
+                    titleStyle={{fontWeight: "bold", fontSize: 13}}
+                    buttonStyle={{backgroundColor: "#bf6240"}}
+                    />
                     </Card>
             </View>
-            <View style={styles.stats}>
-            <View style={styles.graph}>
-  <Text>Number of JavaRewards Customers:</Text>
-  <LineChart
-    data={{
-      labels: ["Jan", "Feb", "Mar", "Apr", "May", "June"],
-      datasets: [
-        {
-          data: [ 10, 20, 30, 40, 10, 33
-          ]
-        }
-      ]
-    }}
-    width={Dimensions.get("window").width * 0.8} // from react-native
-    height={220}
-    yAxisInterval={1} // optional, defaults to 1
-    chartConfig={{
-      backgroundColor: "#e26a00",
-      backgroundGradientFrom: "#fb8c00",
-      backgroundGradientTo: "#ffa726",
-      decimalPlaces: 0, // optional, defaults to 2dp
-      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      style: {
-        borderRadius: 16
-      },
-      propsForDots: {
-        r: "6",
-        strokeWidth: "2",
-        stroke: "#ffa726"
-      }
-    }}
-    bezier
-    style={{
-      marginVertical: 8,
-      borderRadius: 16
-    }}
-  />
-</View>
-                <Text>"customer stats"</Text>
-            </View>
             
+
+            <View style={styles.graph}>
+            <Card containerStyle={{borderRadius: 8}}>
+              <Card.Title style={styles.title}>Stats</Card.Title>
+              <Text>Number of JavaRewards Customers:</Text>
+              <LineChart
+                data={{
+                  labels: ["Jan", "Feb", "Mar", "Apr", "May", "June"],
+                  datasets: [
+                    {
+                      data: [ 10, 20, 30, 40, 10, 33
+                      ]
+                    }
+                  ]
+                }}
+                width={Dimensions.get("window").width * 0.8} // from react-native
+                height={180}
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: "#e26a00",
+                  backgroundGradientFrom: "#fb8c00",
+                  backgroundGradientTo: "#ffa726",
+                  decimalPlaces: 0, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 16
+                  },
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
+                  }
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16
+                }}
+              />
+            </Card>
+            </View>
         </View>
     )
 }
@@ -73,34 +78,45 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
     },
     header: {
         flexDirection: "row",
       width: "100%",
-      marginTop: 60,
+      marginTop: 10,
       marginLeft: 10,
       marginRight: 10,
-      marginBottom: 30,
       paddingLeft: 0,
       gap: 10,
       alignItems: "center",
-      backgroundColor: "yellow"
     },
     bio: {
-        flex: 1
+        marginBottom: 20,
+    },
+    button: {
+      width: "30%",
+      alignSelf: "center",
+      marginTop: 5
+    },
+
+    title: {
+      textAlign: "center",
+      fontWeight: "700",
     },
     stats: {
-        flex: 1
+        flex: 1,
+        backgroundColor: "yellow"
     },
     avatar: {
         margin: 0,
         height: 60,
         width: 60,
         resizeMode: "contain",
-        backgroundColor: "red"
     },
     graph: {
-        flex: 1
+        flex: 1,
+    },
+    businessName: {
+      fontWeight: "700"
     }
 })
